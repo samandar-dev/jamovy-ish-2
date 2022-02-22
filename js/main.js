6d9b7d7f5838f23f5b2699f7a200625fbe7c99a6
akbarinfo <girtbezbet@gmail.com>
2022-02-23 01:00:16 +0500

    CHALA HEADER TO'LIQ EMAS AKANG KALA QO'YDI

--- js/main.js
+++ js/main.js
@@ -1,11 +1,50 @@
-// let elMenuBtn = document.getElementById('menuBtn');
-// let elMenuBox = document.getElementById('menuBox');
-// let elMenuRemov = document.getElementById('menuRemov')
+let openMenuBtn = document.getElementById('id-menu');
+let navBox = document.getElementById('id-navbox');
+let closeMenu = document.getElementById('id-navclose');
+let body = document.getElementById('body');
 
-// elMenuBtn.addEventListener('click', function () {
-//   elMenuBox.classList.add('open-menu');
-// })
 
-// elMenuRemov.addEventListener('click', function () {
-//   elMenuBox.classList.remove('open-menu');
-// })
+openMenuBtn.addEventListener('click', function () {
+  navBox.classList.add('open-nav');
+  body.classList.add('open-left');
+})
+
+closeMenu.addEventListener('click', function () {
+  navBox.classList.remove('open-nav');
+  body.classList.remove('open-left');
+})
+
+
+let openSetingsBtn = document.getElementById('id-menu');
+let boxsetings = document.getElementById('id-setbox');
+let closeSetings = document.getElementById('id-navclose');
+
+openSetingsBtn.addEventListener('click', function () {
+  boxsetings.classList.add('open-settings');
+
+})
+
+closeMenu.addEventListener('click', function () {
+  boxsetings.classList.remove('open-settings');
+})
+
+
+
+let open = document.getElementById('id-setings');
+let navbox = document.getElementById('id-setbox');
+let icon = document.getElementById('id-setclose');
+
+let count = 0;
+open.addEventListener('click', function () {
+  navbox.classList.toggle('open-settings');
+  if (count == 0) {
+    icon.style.fontSize = '32px';
+    icon.className = "bx bx-x";
+    icon.style.color= '#fff'
+    count++;
+  } else {
+    icon.className = "bx bx-cog";
+    icon.style.color= '#fff'
+    count = 0;
+  }
+})
